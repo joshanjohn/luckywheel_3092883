0f4235ed506c9c80876eb74c476516f790a8cf4d
Joshan john <joshanjohn2003@gmail.com>
2025-11-16 16:04:49 +0000

    (chore) improve result card

---
+++ app/src/main/java/com/griffith/luckywheel/models/data/ResultThemeData.kt
@@ -0,0 +1,10 @@
+package com.griffith.luckywheel.models.data
+
+import androidx.compose.ui.graphics.Color
+
+data class ResultThemeData(
+    val primaryColor: Color,
+    val title: String,
+    val emoji: String,
+    val buttonText: String
+)
