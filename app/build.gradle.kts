36a5ae658692961f919d673f75b8f43d7ee8423f
Joshan john <joshanjohn2003@gmail.com>
2025-11-20 20:38:19 +0000

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,7 +84,7 @@ android {
 
 dependencies {
     implementation("com.google.android.libraries.identity.googleid:googleid:1.1.1")
-    implementation("androidx.credentials:credentials:1.1.0")
+    implementation("androidx.credentials:credentials:1.3.0")
     implementation("com.google.android.gms:play-services-auth:21.2.0")
     implementation("androidx.credentials:credentials-play-services-auth:1.3.0")
     implementation("androidx.datastore:datastore-preferences:1.1.7")
